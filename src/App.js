2114e766344f901a097fbda083febd71fe854874
KrishnaManmayi <manmayi.p9@gmail.com>
2022-01-02 00:42:12 +0530

    Designed Result Page Layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rules from "./components/Rules";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import YouPicked from "./components/YouPicked";
+import Result from "./components/Result";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Pick />} />
         <Route path="/youpicked" element={<YouPicked />} />
         <Route path="/housepicked" element={<HousePicked />} />
+        <Route path="/result" element={<Result />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
       <Rules />
---
+++ src/components/Result.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./Result.module.css";
+
+import PaperIcon from "./UI/PaperIcon";
+import RockIcon from "./UI/RockIcon";
+
+const Result = () => {
+  return (
+    <div className={styles.resultContainer}>
+      <p>YOU PICKED</p>
+      <p></p>
+      <p>THE HOUSE PICKED</p>
+      <div className={styles.icon}>
+        <PaperIcon />
+      </div>
+      <div className={styles.resultBox}>
+        <p>YOU WIN</p>
+        <button>Play Again</button>
+      </div>
+      <div className={styles.icon}>
+        <RockIcon />
+      </div>
+    </div>
+  );
+};
+
+export default Result;
